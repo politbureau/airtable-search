454e2685f4f821bcabb6f4042a9c0cca64708726
politbureau <33848135+politbureau@users.noreply.github.com>
2025-04-07 16:25:41 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <header style={{ backgroundColor: "#ffffff", padding: "12px 20px" }}>
+      <header style={{ backgroundColor: "#0a8078", padding: "12px 20px" }}>
         <img
           src="https://www.metroscg.com/hubfs/Media%20Centre/Logos/PNG%20Logos/Metro%20Supply%20Chain%20Primary%20Logo_Full%20Colour_EN.png"
           alt="Metro Supply Chain Logo"
